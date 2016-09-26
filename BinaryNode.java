6e29b5de872dc34bc4f673d424a58d37ad4c8c17
mschon96 <mschon@unomaha.edu>
2016-09-26 16:20:29 -0500

    Create BinaryNode.java

---
+++ BinaryNode.java
@@ -0,0 +1,19 @@
+
+
+
+public class BinaryNode {
+    public int key;
+    BinaryNode left;
+    BinaryNode right;
+
+    BinaryNode(int x){
+        key = x;
+        left = right= null;
+    }
+
+    BinaryNode(int x, BinaryNode l, BinaryNode r){
+        key= x; left = l; right = r;
+    }
+
+}
+~    
